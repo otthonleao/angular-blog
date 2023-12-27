d2ac41ba657624f82f2fa5c66eeae7c1e3e5e9ff
Otthon Leão <otthonleao@hotmail.com>
2023-12-27 11:07:26 -0400

    feat: add content dynamic in content component

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+	
+	@Input()
+	photoCover:string = "https://techwise.co.in/assets/images/services/angular2.png"
+	@Input()
+	contentTitle:string = "Angular is the best framework for corporations system"
+	@Input()
+	contentDescription:string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis saepe enim non iusto. Velit reprehenderit laboriosam hic nobis! Esse, autem qui? Incidunt ab nulla porro laborum fugit inventore sit placeat?"
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
