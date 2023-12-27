c7388ba82c62bf6fbc8c1461495191c269755c99
Otthon Leão <otthonleao@hotmail.com>
2023-12-27 11:15:09 -0400

    feat: add  with uid=1000(otthon) gid=1000(otthon) groups=1000(otthon),4(adm),20(dialout),24(cdrom),25(floppy),27(sudo),29(audio),30(dip),44(video),46(plugdev),116(netdev),119(libvirt),999(docker) for route dynamic with uid=1000(otthon) gid=1000(otthon) groups=1000(otthon),4(adm),20(dialout),24(cdrom),25(floppy),27(sudo),29(audio),30(dip),44(video),46(plugdev),116(netdev),119(libvirt),999(docker)

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+	{
+		"id": 1,
+		"photo": "link photo",
+		"title": "title content",
+		"description": "content"
+	},
+	{
+		"id": 2,
+		"photo": "link photo",
+		"title": "title content",
+		"description": "content"
+	},
+	{
+		"id": 3,
+		"photo": "link photo",
+		"title": "title content",
+		"description": "content"
+	},
+	{
+		"id": 4,
+		"photo": "link photo",
+		"title": "title content",
+		"description": "content"
+	}
+]
