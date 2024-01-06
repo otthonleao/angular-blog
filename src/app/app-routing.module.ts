7f3136f9f7ce8d1fe754c7798ddb4972e7de093b
Otthon Leão <otthonleao@hotmail.com>
2024-01-05 23:13:50 -0400

    feat: add contatos component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		component: HomeComponent
-	},
-	{
-		path: 'content/:id',
-		component: ContentComponent
-	}
+	{ path: '', component: HomeComponent, pathMatch:'full' },
+	{ path: 'content/:id', component: ContentComponent, pathMatch: 'prefix' },
+	{ path: 'contatos', component: ContatosComponent, pathMatch: 'prefix'},
+	{ path: '**', redirectTo: ''} // Rota "coringa": Se alguém utilizar uma rota que não existe, podemos enviar para uma página padrão, nesse caso está indo pra home.
 ]
 
 @NgModule({
